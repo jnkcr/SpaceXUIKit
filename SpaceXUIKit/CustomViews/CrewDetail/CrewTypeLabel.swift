ea9761e3377b3b9bc6869e50ced65cbe2a55b0e7
Jan Kučera <kucera.jan@hotmail.cz>
2022-02-21 14:56:00 +0100

    ":" added at the end of type title

---
+++ SpaceXUIKit/CustomViews/CrewDetail/CrewTypeLabel.swift
@@ -0,0 +1,25 @@
+//
+//  CrewTypeLabel.swift
+//  SpaceXUIKit
+//
+//  Created by Jan Kučera on 21.02.2022.
+//
+
+import UIKit
+
+class CrewTypeLabel: UILabel {
+
+    init(titled title: String) {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        font = UIFont.preferredFont(forTextStyle: .body)
+        textAlignment = .natural
+        textColor = .secondaryLabel
+        text = title + ":"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
